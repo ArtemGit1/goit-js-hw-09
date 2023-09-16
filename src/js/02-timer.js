65aaac08a97232e1dec66fe323073cba55fad670
ArtemGit1 <131163813+ArtemGit1@users.noreply.github.com>
2023-09-16 12:31:37 +0300

    123

---
+++ src/js/02-timer.js
@@ -0,0 +1,87 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const dateInput = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysElement = document.querySelector("[data-days]");
+const hoursElement = document.querySelector("[data-hours]");
+const minutesElement = document.querySelector("[data-minutes]");
+const secondsElement = document.querySelector("[data-seconds]");
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  daysElement.textContent = addLeadingZero(days);
+  hoursElement.textContent = addLeadingZero(hours);
+  minutesElement.textContent = addLeadingZero(minutes);
+  secondsElement.textContent = addLeadingZero(seconds);
+}
+
+function calculateTimeDifference() {
+  const currentDate = new Date();
+  const selectedDateValue = dateInput.value;
+  const selectedDate = new Date(selectedDateValue);
+
+  if (selectedDate <= currentDate) {
+    alert("Please choose a date in the future");
+    return;
+  }
+
+  const timeDifference = selectedDate - currentDate;
+  const timeValues = convertMs(timeDifference);
+
+  if (timeValues.days === 0 && timeValues.hours === 0 && timeValues.minutes === 0 && timeValues.seconds === 0) {
+    clearInterval(timerInterval);
+    alert("Countdown completed!");
+    startButton.disabled = false;
+  } else {
+    updateTimerDisplay(timeValues);
+  }
+}
+
+startButton.addEventListener("click", () => {
+  calculateTimeDifference();
+  // Додайте наступні дві строки для оновлення таймера кожну секунду після натискання кнопки "Start"
+  startTimer(); // Запускаємо таймер
+  timerInterval = setInterval(calculateTimeDifference, 1000); // Оновлюємо таймер кожну секунду
+});
+
+flatpickr("#datetime-picker", {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates.length > 0) {
+      startButton.disabled = false;
+    } else {
+      startButton.disabled = true;
+    }
+  },
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+let timerInterval;
+
+function startTimer() {
+  timerInterval = setInterval(calculateTimeDifference, 1000);
+}
+
+startButton.disabled = true;
+
+calculateTimeDifference();
---
+++ src/js/03-promises.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.form');
+
+  form.addEventListener('submit', function (e) {
+    e.preventDefault(); // Забороняємо формі відправлятися знову
+
+    const delayInput = form.querySelector('input[name="delay"]');
+    const stepInput = form.querySelector('input[name="step"]');
+    const amountInput = form.querySelector('input[name="amount"]');
+
+    const delay = parseInt(delayInput.value);
+    const step = parseInt(stepInput.value);
+    const amount = parseInt(amountInput.value);
+
+    if (isNaN(delay) || isNaN(step) || isNaN(amount)) {
+      alert('Будь ласка, введіть коректні числові значення для всіх полів.');
+      return;
+    }
+
+    // Створюємо і обробляємо проміси
+    for (let i = 0; i < amount; i++) {
+      createPromise(i + 1, delay + i * step)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+    }
+  });
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
